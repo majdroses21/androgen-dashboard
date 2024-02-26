cb34c8370cb67e8af6fdf10680650d7ea646972c
Zeinab Kanaan <zsskanaan@gmail.com>
2024-02-26 16:30:58 +0200

    Add Login Page Design

---
+++ src/views/Login.vue
@@ -0,0 +1,294 @@
+<template>
+    <div class="container-fluid login-container">
+        <div class="box-star">
+            <svg class="star-1" width="80" height="80" viewBox="0 0 80 80" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M40 0C40 0 42.207 20.5997 50.8036 29.1964C59.4003 37.793 80 40 80 40C80 40 59.4003 42.207 50.8036 50.8036C42.207 59.4003 40 80 40 80C40 80 37.793 59.4003 29.1964 50.8036C20.5997 42.207 0 40 0 40C0 40 20.5997 37.793 29.1964 29.1964C37.793 20.5997 40 0 40 0Z" fill="white"/>
+            </svg>
+            <svg class="star-2" width="61" height="52" viewBox="0 0 61 52" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M30.5 -66C30.5 -66 32.1828 -35.6154 38.7378 -22.9354C45.2927 -10.2553 61 -7 61 -7C61 -7 45.2927 -3.74466 38.7378 8.93536C32.1828 21.6154 30.5 52 30.5 52C30.5 52 28.8172 21.6154 22.2622 8.93536C15.7073 -3.74466 0 -7 0 -7C0 -7 15.7073 -10.2553 22.2622 -22.9354C28.8172 -35.6154 30.5 -66 30.5 -66Z" fill="white"/>
+            </svg>
+            <svg class="star-3" width="61" height="60" viewBox="0 0 61 60" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M30.5 0C30.5 0 32.1828 15.4498 38.7378 21.8973C45.2927 28.3447 61 30 61 30C61 30 45.2927 31.6553 38.7378 38.1027C32.1828 44.5502 30.5 60 30.5 60C30.5 60 28.8172 44.5502 22.2622 38.1027C15.7073 31.6553 0 30 0 30C0 30 15.7073 28.3447 22.2622 21.8973C28.8172 15.4498 30.5 0 30.5 0Z" fill="white"/>
+            </svg>
+            <svg class="star-4" width="80" height="80" viewBox="0 0 80 80" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M40 0C40 0 42.207 20.5997 50.8036 29.1964C59.4003 37.793 80 40 80 40C80 40 59.4003 42.207 50.8036 50.8036C42.207 59.4003 40 80 40 80C40 80 37.793 59.4003 29.1964 50.8036C20.5997 42.207 0 40 0 40C0 40 20.5997 37.793 29.1964 29.1964C37.793 20.5997 40 0 40 0Z" fill="white"/>
+            </svg>
+            <svg class="star-5" width="80" height="118" viewBox="0 0 80 118" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M40 0C40 0 42.207 30.3846 50.8036 43.0646C59.4003 55.7447 80 59 80 59C80 59 59.4003 62.2553 50.8036 74.9354C42.207 87.6154 40 118 40 118C40 118 37.793 87.6154 29.1964 74.9354C20.5997 62.2553 0 59 0 59C0 59 20.5997 55.7447 29.1964 43.0646C37.793 30.3846 40 0 40 0Z" fill="white"/>
+            </svg>
+        </div>
+        <div class="box-star-2">
+            <svg class="star-2-1" width="60" height="80" viewBox="0 0 60 80" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M40 0C40 0 42.207 20.5997 50.8036 29.1964C59.4003 37.793 80 40 80 40C80 40 59.4003 42.207 50.8036 50.8036C42.207 59.4003 40 80 40 80C40 80 37.793 59.4003 29.1964 50.8036C20.5997 42.207 0 40 0 40C0 40 20.5997 37.793 29.1964 29.1964C37.793 20.5997 40 0 40 0Z" fill="white"/>
+            </svg>
+            <svg class="star-2-3" width="80" height="80" viewBox="0 0 80 80" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M40 0C40 0 42.207 20.5997 50.8036 29.1964C59.4003 37.793 80 40 80 40C80 40 59.4003 42.207 50.8036 50.8036C42.207 59.4003 40 80 40 80C40 80 37.793 59.4003 29.1964 50.8036C20.5997 42.207 0 40 0 40C0 40 20.5997 37.793 29.1964 29.1964C37.793 20.5997 40 0 40 0Z" fill="white"/>
+            </svg>
+            <svg class="star-2-4" width="61" height="60" viewBox="0 0 61 60" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M30.5 0C30.5 0 32.1828 15.4498 38.7378 21.8973C45.2927 28.3447 61 30 61 30C61 30 45.2927 31.6553 38.7378 38.1027C32.1828 44.5502 30.5 60 30.5 60C30.5 60 28.8172 44.5502 22.2622 38.1027C15.7073 31.6553 0 30 0 30C0 30 15.7073 28.3447 22.2622 21.8973C28.8172 15.4498 30.5 0 30.5 0Z" fill="white"/>
+            </svg>
+            <svg class="star-2-5" width="80" height="118" viewBox="0 0 80 118" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M40 0C40 0 42.207 30.3846 50.8036 43.0646C59.4003 55.7447 80 59 80 59C80 59 59.4003 62.2553 50.8036 74.9354C42.207 87.6154 40 118 40 118C40 118 37.793 87.6154 29.1964 74.9354C20.5997 62.2553 0 59 0 59C0 59 20.5997 55.7447 29.1964 43.0646C37.793 30.3846 40 0 40 0Z" fill="white"/>
+            </svg>
+            <svg class="star-2-2" width="80" height="75" viewBox="0 0 80 75" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M40 0C40 0 42.207 30.3846 50.8036 43.0646C59.4003 55.7447 80 59 80 59C80 59 59.4003 62.2553 50.8036 74.9354C42.207 87.6154 40 118 40 118C40 118 37.793 87.6154 29.1964 74.9354C20.5997 62.2553 0 59 0 59C0 59 20.5997 55.7447 29.1964 43.0646C37.793 30.3846 40 0 40 0Z" fill="white"/>
+            </svg>
+        </div>
+        <div class="login-form">
+            <img class="" src="../assets/images/logo.png" alt="">
+            <div class="login-title">Login to our dashboard </div>
+            <form class="form-style">
+                <label class="label-style" for="user-name">
+                    <input class="input-style" type="text" id="user-name" name="user-name" placeholder="Username" v-model="userName">
+                    <i class="fa-solid fa-user login-icon"></i>
+                    <div class="error-msg" v-if="false">
+                        <div class="error-txt">
+                            <i class="fa-solid fa-exclamation error-icon"></i>
+                        </div>
+                        <span>Incorrect username</span>
+                    </div>
+                </label>
+                <label class="label-style" for="password">
+                    <input class="input-style" type="password" id="password" name="password" placeholder="Password" v-model="password">
+                    <i class="fa-solid fa-lock login-icon"></i> 
+                    <div v-if="false" class="error-msg">
+                        <div class="zz">
+                            <i class="fa-solid fa-exclamation error-icon"></i>
+                        </div>
+                        <span>Incorrect username</span>
+                    </div>           
+                </label>
+                <div class="d-flex checkbox-box" >
+                    <input class="checkbox-style" type="checkbox" id="remember" name="remember" v-model="isChecked">
+                    <label class="label-style-2" for="remember">Remember me</label>
+                </div>
+                <button class="button-login" type="button">Login</button>
+            </form>
+        </div>  
+    </div>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                userName :'',
+                password:'',
+                isChecked:false
+            }
+        }
+    }
+</script>
+<style scoped>
+.error-txt {
+    width: 14px;
+    height: 14px;
+    border: 1px solid;
+    border-radius: 20px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.error-icon {
+    font-size: 8px !important;
+}
+.error-msg {
+    font-size: 12px;
+    font-weight: 400;
+    color: #EB5757;
+    margin-top: 5px;
+    margin-inline: 23px;
+    display: flex;
+    gap: 10px;
+    align-items: baseline;
+}
+.box-star {
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    width: 217px;
+    height: 227px;
+}
+.box-star-2{
+    position: absolute;
+    bottom: 0px;
+    right: 0px;
+    width: 197px;
+    height: 274px;
+}
+.login-container {
+    width: 100%;
+    height: 100vh;
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: rgb(66,106,179);
+    background: linear-gradient(180deg, rgba(66,106,179,1) 34%, rgba(51,51,51,1) 100%);
+  
+}
+.login-form {
+    background-color: white;
+    width: 470px;
+    border-radius: 20px;
+    padding: 32px 0px;
+    max-width: 90%;
+    z-index: 1;
+}
+.login-form img {
+    width: 50%;
+    margin: auto;
+    display: block;
+}
+.login-title {
+    color: var(--main-color);
+    font-size: 18px;
+    font-weight: 400;
+    text-align: center;
+    margin:32px 0px;
+    position: relative;
+}
+.label-style {
+    display: block;
+    margin: auto;
+    /* width: 250px; */
+    position: relative;
+    margin-bottom: 16px;
+
+}
+.input-style {
+    padding: 10px 46px;
+    border-radius: 30px;
+    border: 1px solid  #8080806b;
+    margin: auto;
+    display: block;
+    font-size: 14px;
+    font-weight: 400;
+    width: 100%;
+}
+.input-style:focus-visible {
+    outline: none;
+}
+.input-style::placeholder {
+    color:  #8080806b;
+}
+.login-icon {
+    position: absolute;
+    top: 13px;
+    left: 26px;
+    color:  #8080806b;
+    z-index: 1;
+}
+.label-style-2 {
+    color: var(--main-color-2);
+    font-size: 14px;
+    font-weight: 500;
+    position: relative;
+    bottom: 1px;
+    margin-inline: 5px;
+    
+}
+.form-style {
+    width: 75%;
+    max-width: 75%;
+    margin: auto;
+}
+.checkbox-style {
+    width: 15px;
+    height: 15px;
+    border-radius: 6px;
+    margin-inline: 2px;
+    
+}
+.checkbox-box {
+    width: fit-content;
+    position: relative;
+    left: 25px;
+}
+.button-login {
+    background-color: var(--secondary-color);
+    padding: 14px 26px;
+    border: none;
+    border-radius: 30px;
+    color: white;
+    width: 100%;
+    font-size: 14px;
+    font-weight: 500;
+}
+.checkbox-box {
+    margin-bottom: 16px;
+}
+.button-login:hover {
+    background-color: var(--hover-color);
+
+}
+.star-1,.star-2,.star-3,.star-4,.star-5,.star-2-1,.star-2-2,.star-2-3,.star-2-4,.star-2-5{
+    position: absolute;
+    animation-name: star;
+    animation-duration: 2s;
+    opacity: 0.1;
+    animation-iteration-count: infinite; 
+}
+.star-1 {
+    top: 34px;
+    left: 66px;
+}
+.star-2 {
+    left: 120px;
+    animation-delay: 0.4s;
+}
+.star-3 {
+    left: 12px;
+    top: 10px;
+    animation-delay: 0.8s;
+}
+.star-4 {
+    top: 85px;
+    left: 131px; 
+    animation-delay: 1.2s;
+}
+.star-5 {
+    top: 108px;
+    left: 0px;
+    animation-delay: 1.6s;
+}
+.star-2-1 {
+    top: 177px;
+    right: 0px;
+}
+.star-2-2 {
+    bottom: 0px;
+    left: 27px;
+    animation-delay: 0.4s; 
+}
+.star-2-3 {
+    top: 120px;
+    left: 75px;
+    animation-delay: 0.8s;
+
+}
+.star-2-4 {
+    top: 80px;
+    left: 7px;
+    animation-delay: 1.2s;
+}
+.star-2-5 {
+    top: 0px;
+    right: 0px;
+    animation-delay: 1.6s;
+}
+.error-style {
+    border-color: #EB5757;
+}
+@keyframes star {
+    0%   { opacity: 0.5;}
+    15%   { opacity: 0.5;}
+    20%   { opacity: 0.1;}
+    90%   { opacity: 0.1;}
+    100%   { opacity: 0.5;}
+}
+@media(max-width:425px) {
+    .login-form img {
+        width: 60%;
+    }
+}
+</style>
