db9b6336597e0363a724cbacb1ed58635805586c
Majd Ahmed <majdrose@gmail.com>
2025-05-03 22:48:42 +0300

    Make the code cleaner

--- src/components/icons/TimeAlert.vue
+++ src/components/icons/TimeAlert.vue
@@ -1,9 +1,9 @@
 <template>
     <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M16.6667 2.5H3.33333C2.8731 2.5 2.5 2.8731 2.5 3.33333V16.6667C2.5 17.1269 2.8731 17.5 3.33333 17.5H16.6667C17.1269 17.5 17.5 17.1269 17.5 16.6667V3.33333C17.5 2.8731 17.1269 2.5 16.6667 2.5Z" fill="#F58220" fill-opacity="0.1"/>
-    <path d="M10 17.5H3.33333C3.11232 17.5 2.90036 17.4122 2.74408 17.2559C2.5878 17.0996 2.5 16.8877 2.5 16.6667V3.33333C2.5 3.11232 2.5878 2.90036 2.74408 2.74408C2.90036 2.5878 3.11232 2.5 3.33333 2.5H16.6667C16.8877 2.5 17.0996 2.5878 17.2559 2.74408C17.4122 2.90036 17.5 3.11232 17.5 3.33333V10" stroke="#F58220" stroke-linecap="round" stroke-linejoin="round"/>
-    <path d="M14.168 13.166V15.666" stroke="#F58220" stroke-linecap="round" stroke-linejoin="round"/>
-    <path d="M8.33301 11.6673L9.99967 10.0007V5.83398" stroke="#F58220" stroke-linecap="round" stroke-linejoin="round"/>
-    <path d="M14.125 17.084H14.2083" stroke="#F58220" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M16.6667 2.5H3.33333C2.8731 2.5 2.5 2.8731 2.5 3.33333V16.6667C2.5 17.1269 2.8731 17.5 3.33333 17.5H16.6667C17.1269 17.5 17.5 17.1269 17.5 16.6667V3.33333C17.5 2.8731 17.1269 2.5 16.6667 2.5Z" fill="#426AB3" fill-opacity="0.1"/>
+    <path d="M10 17.5H3.33333C3.11232 17.5 2.90036 17.4122 2.74408 17.2559C2.5878 17.0996 2.5 16.8877 2.5 16.6667V3.33333C2.5 3.11232 2.5878 2.90036 2.74408 2.74408C2.90036 2.5878 3.11232 2.5 3.33333 2.5H16.6667C16.8877 2.5 17.0996 2.5878 17.2559 2.74408C17.4122 2.90036 17.5 3.11232 17.5 3.33333V10" stroke="#426AB3" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M14.168 13.166V15.666" stroke="#426AB3" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M8.33301 11.6673L9.99967 10.0007V5.83398" stroke="#426AB3" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M14.125 17.084H14.2083" stroke="#426AB3" stroke-linecap="round" stroke-linejoin="round"/>
     </svg>
 </template>
--- src/views/CourseDetails.vue
+++ src/views/CourseDetails.vue
@@ -332,8 +332,8 @@
             </EasyDataTable>
         </div>
     </div>
-    </template>
-    <script>
+</template>
+<script>
         import EasyDataTable from 'vue3-easy-data-table';
         import {api_url,storage_url} from '../constants';
         import useVuelidate from '@vuelidate/core';
@@ -465,7 +465,7 @@
            { text: this.$t('lecturesCount'), value:"lectures_count", height:'44' },
            { text: this.$t('length'), value:"length", height:'44' },
            { text: this.$t('order'), value:"order", height:'44' },
-           { text: '', value:"manage", height:'44' }
+           { text: 'Actions', value:"manage", height:'44' }
         ]
        },
        headers2() {
@@ -776,351 +776,143 @@
 
         }
     }
-    </script>
-    
-    
-    
-    <style scoped>
-     .vue3-easy-data-table {
-        z-index: 0 !important;
-    }
-    .img_user {
-        width: 20px;
-        height: 20px;
-    }
-    .img_user img {
-        width: 100%;
-        height: 100%;
-        object-fit: cover; 
-    }
-    .not-found {
-        display: flex;
-        justify-content: center;
-        align-items:center;
-        flex-direction:column;
-        margin-top: 20px;
-    }
-    .data_table:deep() .shadow {
-        box-shadow: none!important;
-    }
-    .box-modal {
-         display: flex;
-    }
-    .no-lesson {
-        font-weight :500px;
-        color :var(--main-color);
-        margin-top:16px;
-        margin-bottom:24px;
-    }
-     .details_box {
-        background-color: white;
-        padding: 24px 20px;
-        border-radius: 30px;
-     }
-     .add-btn {
-        cursor: pointer;
-     }
-     .sec-head {
-        display: flex;
-        justify-content: space-between;
-     }
-     .sec-head-2 {
-        display: flex;
-        gap: 24px;
-     }
-    .box-title {
-        justify-content: unset;
-        gap: 16px;
-    }
-    .user-img {
-        width: 20px;
-        height: 20px;
-        border-radius: 30px;
-    }
-    .add-icon :deep() path {
-        stroke: var(--primary-color);
-    }
-    .user-img img {
-        object-fit: cover;
-    }
-     .det_title {
-        font-size: 20px;
-        font-weight: 600;
-        color: var(--main-color);
-        margin:8px 0px 16px 0px;
-     }
-     .lessons {
-        font-weight: 600;
-        font-size: 20px;
-        color: var(--main-color);
-     }
-     .info-flex {
-        display: flex;
-        gap: 16px;
-     }
-     .arrow-icon {
-        color: #3B424A;
-    
-     }
-     .edit {
-        font-size: 16px;
-        font-weight: 400;
-        color: var(--primary-color);
-     }
-     .edit_icon :deep() path {
-        stroke: var(--primary-color);
-     }
-     .edit_icon {
-        width: 15px;
-        height: 15px;
-     }
-    
-     .user_img {
-        width: 20px;
-        height: 20px;
-     }
-     .info {
-        color: #7B8190;
-        font-size: 14px;
-        font-weight: 400;
-        text-align: justify;
-     }
-    .modal_content {
-     min-height: 533px;
-     height: 533px;
-    }
-    .modal_body {
-     overflow-y: auto;
-    }
-    .label-style {
-        display: block;
-        margin: auto;
-        position: relative;
-        margin-bottom: 4px;
-        margin-left: 0px;
-     }
-     .edit-btn {
-        cursor: pointer;
-     }
-     .input-style {
-        padding-inline: 16px;
-        border-radius: 8px;
-     }
-     .button-style-modal {
-      padding: 12px 58px;
-      text-wrap:nowrap;
-    }
-    .button-style {
-        padding: 10px 13px;
-     }
-     .button-style-2 {
-        background-color: white;
-        color: var(--primary-color);
-        border: 1px solid var(--primary-color);
-     }
-     .button-style-2:hover {
-        background-color: var(--primary-color);
-        color: white;
-     }
-     .select-style-modal:deep() .vs__dropdown-toggle {
-     border: none !important;
-    }
-    .select-style-modal:deep() .vs__search {
-     margin-top: 0px;
-    }
-    .select-style-modal :deep()::placeholder {
-     color: #d9d9d9;
-    }
-    .select-style-modal :deep() .v-select{
-     max-height: 42.6px !important;
-     height: 42.6px!important;
-     padding-top: 7px;
-     padding-bottom: 7px;
-    }
-    .select-style-modal :deep() .vs__dropdown-option--highlight {
-     background-color: var(--primary-color) !important;
-    }
-    
-    .select-style-modal :deep() .vs__dropdown-menu {
-     border-radius: 8px;
-     margin-top: 9px;
-    }
-    .button-style-modal {
-      padding: 12px 58px;
-      text-wrap:nowrap;
-    }
-    .add{
-        font-size: 16px;
-        font-weight: 400;
-        color: var(--primary-color);
-    
-    }
-    .data_table :deep() .vue3-easy-data-table__main.border-cell .vue3-easy-data-table__body td {
-      border-right: none;
-      padding: 16px 24px;
-    }
-    [data-direction = rtl] .data_table :deep() .vue3-easy-data-table__main.border-cell .vue3-easy-data-table__body td {
-       text-align: right;
-    }
-    .data_table :deep() .vue3-easy-data-table__main.border-cell .vue3-easy-data-table__header th {
-      border-right: none !important;
-      font-weight: 500;
-    }
-    .data_table :deep() .vue3-easy-data-table__header th {
-      background-color: rgb(246 248 251);
-    }
-    .data_table :deep() .easy-data-table__rows-selector .rows-input__wrapper {
-      justify-content: unset;
-      gap: 9px;
-      width: 36px;
-      padding: 0px;
-    }
-    .data_table :deep() .vue3-easy-data-table__footer .pagination__items-index {
-      margin-left: 0px;
-    }
-    .data_table :deep() .vue3-easy-data-table__footer {
-      border-bottom-left-radius: 12px;
-      border-bottom-right-radius: 12px;
-    }
-    .data_table_admin :deep() .vue3-easy-data-table__main {
-       max-height: calc(100vh - 302px);
-       height: calc(100vh - 302px);
-    }
-    .data_table_height :deep() .vue3-easy-data-table__main {
-       max-height: calc(100vh - 302px);
-       height: calc(100vh - 302px);
-    }
-    .required-icon :deep() path {
-      fill: red;
-    }
-    .lesson-det {
-        font-weight: bold;
-    }
-    [data-direction = rtl] .data_table :deep().vue3-easy-data-table__main {
-        direction: rtl;
-    }
-    [data-direction = rtl].data_table :deep() .table-box-btn  {
-       justify-content: flex-start;
-    }
-    [data-direction = rtl].data_table :deep() .vue3-easy-data-table__footer{
-       justify-content: flex-start;
-    }
-    [data-direction = rtl].data_table :deep() .vue3-easy-data-table__footer .pagination__items-index{
-        margin: 0 10px 0 20px;
-    }
-    [data-direction = rtl].data_table:deep() .vue3-easy-data-table__footer{
-       flex-direction: row-reverse;
-    }
-    [data-direction = rtl] .data_table :deep() .previous-page__click-button {
-       transform: rotate(180deg);
-    
-    }
-    [data-direction = rtl] .data_table :deep() .next-page__click-button {
-        transform: rotate(180deg);
-    }
-    [data-direction = rtl] .lesson-det {
-        flex-direction: row-reverse;
-    }
-    .btn_table {
-      border: none;
-      background-color: transparent;
-    }
-    .btn_table:hover :deep() .table-icon path {
-      stroke: var(--primary-color);
-    
-    }
-    .data_table :deep() .easy-data-table__rows-selector ul.select-items{
-      position: absolute;
-      top: 23px;
-      left: -3px; 
-      width: 44px;
-    }
-    .data_table :deep() .vue3-easy-data-table__main {
-      border-top-right-radius: 12px;
-      border-top-left-radius: 12px;
-    }
-    .table-icon {
-      cursor: pointer;
-    }
-    
-    
-    
-    /* rtl */
-    [data-direction = rtl] .select-style-modal :deep() .vs__dropdown-option {
-       text-align: right;
-    }
-    [data-direction = rtl] .select-style-modal :deep() .vs__dropdown-option--highlight {
-       text-align: right;
-    }
-    
-    [data-direction = rtl] .arrow-icon {
-        transform: rotate(180deg);
-    }
-    ::-webkit-scrollbar {
-    width: 3px;
-    }
-    
-    /* Track */
-    ::-webkit-scrollbar-track {
-    box-shadow: inset 0 0 3px grey;
-    }
-    
-    /* Handle */
-    ::-webkit-scrollbar-thumb {
-    background: var(--primary-color);
-    border-radius: 10px;
-    }
-    [data-direction = rtl] .data_table :deep() .next-page__click-button {
+</script>
+<style scoped src="../assets/css/TabStyle.css"> </style>
+<style scoped>
+/* 1. Image and Appearance Classes */
+.img_user {
+    width: 20px;
+    height: 20px;
+}
+.img_user img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover; 
+}
+.user-img {
+    width: 20px;
+    height: 20px;
+    border-radius: 30px;
+}
+.user-img img {
+    object-fit: cover;
+}
+.user_img {
+    width: 20px;
+    height: 20px;
+}
+/* 2. Messages and alerts classes */
+.not-found {
+    display: flex;
+    justify-content: center;
+    align-items:center;
+    flex-direction:column;
+    margin-top: 20px;
+}
+.no-lesson {
+    font-weight: 500;
+    color: var(--main-color);
+    margin-top: 16px;
+    margin-bottom: 24px;
+}
+/* 3. General coordination and elements classes */
+.box-modal {
+    display: flex;
+}
+.details_box {
+    background-color: white;
+    padding: 24px 20px;
+    border-radius: 30px;
+}
+.sec-head {
+    display: flex;
+    justify-content: space-between;
+}
+.sec-head-2 {
+    display: flex;
+    gap: 24px;
+}
+.info-flex {
+    display: flex;
+    gap: 16px;
+}
+.det_title {
+    font-size: 20px;
+    font-weight: 600;
+    color: var(--main-color);
+    margin: 8px 0px 16px 0px;
+}
+.lessons {
+    font-weight: 600;
+    font-size: 20px;
+    color: var(--main-color);
+}
+.info {
+    color: #7B8190;
+    font-size: 14px;
+    font-weight: 400;
+    text-align: justify;
+}
+/* 4. Button classes and interactions */
+.add-btn {
+    cursor: pointer;
+}
+.edit-btn {
+    cursor: pointer;
+}
+.add-icon :deep() path {
+    stroke: var(--primary-color);
+}
+.arrow-icon {
+    color: #3B424A;
+}
+.edit {
+    font-size: 16px;
+    font-weight: 400;
+    color: var(--primary-color);
+}
+.edit_icon :deep() path {
+    stroke: var(--primary-color);
+}
+.edit_icon {
+    width: 15px;
+    height: 15px;
+}
+.add {
+    font-size: 16px;
+    font-weight: 400;
+    color: var(--primary-color);
+}
+.lesson-det {
+    font-weight: bold;
+}
+/* 5. Additional table classes */
+.data_table:deep() .shadow {
+    box-shadow: none!important;
+}
+.data_table_admin :deep() .vue3-easy-data-table__main {
+    max-height: calc(100vh - 302px);
+    height: calc(100vh - 302px);
+}
+.data_table_height :deep() .vue3-easy-data-table__main {
+    max-height: calc(100vh - 302px);
+    height: calc(100vh - 302px);
+}
+/* 6. Additional RTL classes */
+[data-direction = rtl] .lesson-det {
+    flex-direction: row-reverse;
+}
+[data-direction = rtl] .arrow-icon {
     transform: rotate(180deg);
 }
-    [data-direction=rtl] .data_table :deep() .vue3-easy-data-table__footer .pagination__rows-per-page{
-    direction: ltr;
-    }
-    [data-direction=rtl] .data_table :deep().vue3-easy-data-table__footer .pagination__items-index {
-    direction: ltr;
-    margin: 0px 10px 0 20px;
-    }
-    @media(max-width:1024px) {
-        .details_box {
-            margin-inline: 5px;
-        }
-    }
-    @media(max-width: 576px) {
-        .modal_content {
-            padding: 15px 8px;
-        }
-        .modal-dialog-style {
-        width: 90%;
-        max-width: 90%;
-        margin-inline: auto;
-        }
-        .main-box {
-        padding: 22px 11px;
-        }
-        .main-box {
-            padding-bottom: 2px;
-        }
-        .info-flex {
-            flex-direction: column;
-            gap: 6px;
-        }
-        .det_title {
-            margin-bottom: 12px;
-        }
-        .button-style{
-            padding: 7px 19px;
-        }
-        .box-modal {
-            flex-direction: column;
-        }
-    }
-    .modal-dialog{
-        max-width:600px
-    }
-    .modal_content_lesson_info{
-        min-height: 211px;
-        height: 211px;
-        padding:20px;
-    }
+/* 7. Additional modal classes */
+.modal-dialog {
+    max-width: 600px;
+}
+.modal_content_lesson_info {
+    min-height: 211px;
+    height: 211px;
+    padding: 20px;
+}
 
-    </style>
+</style>
